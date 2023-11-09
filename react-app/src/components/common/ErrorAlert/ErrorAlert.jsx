25f1dc3d66fe68f020762cdf629ba0ae89062e68
smoothwizz <a.popa@goit.global>
2023-11-09 12:29:45 +0200

    Update alert box

--- react-app/src/components/common/ErrorAlert/ErrorAlert.jsx
+++ react-app/src/components/common/ErrorAlert/ErrorAlert.jsx
@@ -1,17 +1,35 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
 import styles from "./ErrorAlert.module.css";
+import { HiX } from "react-icons/hi";
 
 export default class ErrorAlert extends Component {
   static propTypes = { errors: PropTypes.string.isRequired };
 
+  state = {
+    isClosed: false,
+  };
+
   render() {
     const { errors } = this.props;
+    const { isClosed } = this.state;
 
-    if (errors.length === 0) {
+    if (errors.length === 0 || isClosed) {
       return;
     }
 
-    return <div className={styles.alert}>{errors}</div>;
+    return (
+      <section className={styles.alert}>
+        <div className={styles.content}>
+          <p>{errors}</p>
+          <span
+            className={styles.close}
+            onClick={() => this.setState({ isClosed: true })}
+          >
+            <HiX />
+          </span>
+        </div>
+      </section>
+    );
   }
 }
